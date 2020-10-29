70841a04d8710a88b2e38dd0f591aec476f78aa0
lily1215z <60219984+lily1215z@users.noreply.github.com>
2020-10-29 13:19:31 +0300

    Add files via upload

--- lesson13 comment jQuery/maun2.js
+++ lesson13 comment jQuery/maun2.js
@@ -1,27 +1,28 @@
 "use strict";
 
 $(function() {
-
-    const btn = $(".btn");
     const commentName = $(".name");
     const commentBody = $(".comment");
     const commentField = $(".list-section");
     let time = moment().format("MMM Do YYYY"); 
 
     let comments = [];
-    btn.on("click", function(e) {
+    $(".btn").on("click", function(e) {
         e.preventDefault();
         let comment = {
-            name: commentName.value,
-            body: commentBody.value,
+            name: commentName.val(),
+            body: commentBody.val(),
             time: time.valueOf()
         };
-        commentBody.value = "";
-        commentName.value = "";
+        console.log(comment);
+        commentName.val("");
+        commentBody.val("");
         comments.push(comment);
         saveComments();
         showComments();
+        
     });
+
     function saveComments() {
         localStorage.setItem("comments", JSON.stringify(comments));
     }
@@ -35,13 +36,14 @@ $(function() {
     loadComments();
 
     function showComments() { 
-        let out ="";
+        let out = "";
         $.each(comments, function(item) {
-            out += `<p>${item.name}</p>`;
-            out += `<p>${item.body}</p>`;
-            out += `<p>${item.time}</p>`;
+            out += `<p class="color-name">${item.name}</p>`;
+            // $(".list-section").append("<p>" + item.name.value + "</p>");
+            out += `<p class="color-body">${item.body}</p>`;
+            out += `<p class="color-time">${item.time}</p>`;
         });
-        commentField.html = out;
+        commentField.html(out);
     }
 
 });
